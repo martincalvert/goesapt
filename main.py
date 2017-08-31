efdfe63cb1c135aa39eb1e4bcf94ea9c235c0a86
Calvert <martin.calvert@disney.com>
2017-08-31 16:22:00 -0700

    Single code search and headless

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from splinter import Browser
 from time import sleep
 from sys import argv
+from selenium import webdriver
 import re
 import os
 
 CODES = {
     'LAX': 5180,
-    'SFO': 5446,
     'LBC': 8920
 }
 
@@ -27,16 +27,18 @@ months = [
 
 def run_script(browser, code):
     log_in(browser)
-    res = check_appointment(browser, CODES[code])
-    if res is None:
+    result = check_appointments(browser, CODES[code])
+    if result is None:
         print 'no appointment'
-    print res
+    else:
+        print result
 
 def get_date(browser):
     text = ' '.join(node.text for node in browser.find_by_css('.background .header .date td')).lower()
     year = None
     month = None
     day = None
+    print('got a date')
     for part in re.split('[^a-z0-9]+', text):
         isint = False
         try:
@@ -62,52 +64,56 @@ def get_date(browser):
 def log_in(browser):
     url = "https://goes-app.cbp.dhs.gov/goes/HomePagePreAction.do"
     browser.visit(url)
-    browser.fill('j_username', os.environ['USERNAME'])
-    browser.fill('j_password', os.environ['PASSWORD'])
+    browser.fill('j_username', os.environ['GOES_USERNAME'])
+    browser.fill('j_password', os.environ['GOES_PASSWORD'])
     sign_in_button = browser.find_by_css('#SignIn')
     if not sign_in_button:
         1/0
     sign_in_button.click()
     check_me = browser.find_by_css('#checkMe')
     if not check_me:
-        1/0
+        return
     check_me.click()
 
 
 #Select another center
-def check_appointments(browser, codes):
-    if not codes:
+def check_appointments(browser, code):
+    if not code:
         return
-    manage = browser.find_by_css('[name=manageAptm]')
-    if not manage:
-        1/0
-    manage.click()
-    reschedule = browser.find_by_css('input[name=reschedule]')
-    if not reschedule:
-        1/0
-    reschedule.click()
-    for code in codes:
-        option = browser.find_by_css('input[value="%s"]' % code)
-        if not option:
+    manage = browser.find_by_css('[name=schedInt]')
+    if manage:
+        manage.click()
+    else:
+        reschedule = browser.find_by_css('input[name=reschedule]')
+        if not reschedule:
             1/0
-        option.click()
-        next_button = browser.find_by_css('input[name=next]')
-        if not next_button:
-            1/0
-        next_button.click()
-        if browser.is_text_present('no available'):
-            yield None
-        else:
-            yield get_date(browser)
+        reschedule.click()
 
+    option = browser.find_by_css('input[value="%s"]' % code)
+    if not option:
+        1/0
+    option.click()
+    next_button = browser.find_by_css('input[name=next]')
+    if not next_button:
+        1/0
+    next_button.click()
+    if browser.is_text_present('no available'):
+        return
+    else:
+        return get_date(browser)
 
 def get_browser():
-    return Browser('chrome', executable_path='./chromedriver')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    return Browser('chrome', executable_path='./chromedriver', options=chrome_options)
 
 if __name__ == '__main__':
     with get_browser() as browser:
-        browser = get_browser()
-        code = 'LAX'
-        if len(argv) > 1:
-            code = argv[1]
-        run_script(browser, code)
+        try:
+            browser = get_browser()
+            code = 'LAX'
+            if len(argv) > 1:
+                code = argv[1]
+            run_script(browser, code)
+        finally:
+            browser.quit()
